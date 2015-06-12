4eeaf27e145782fa460aa537f91074171bb1feb3
Michael Owens <michaelowensnl@gmail.com>
2015-06-12 15:40:33 +0200

    Remove commented code

--- lib/models/channel.ex
+++ lib/models/channel.ex
@@ -5,6 +5,7 @@ defmodule Database.Channel do
     field :name, :string, size: 255
     field :live, :boolean
     field :retries, :integer
+
     timestamps
   end
 end
--- lib/models/timer.ex
+++ lib/models/timer.ex
@@ -4,7 +4,7 @@ defmodule Database.Timer do
   schema "timer" do
     field :cmd, :string, size: 255
     field :interval, :integer
-    # field :last_run, :datetime, default: Ecto.DateTime.local
+
     timestamps
   end
 end
