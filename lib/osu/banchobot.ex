ce84189edab28244fe2395e43bfee1364059bd42
Nicholas Tay <n2468txd@users.noreply.github.com>
2015-06-28 08:51:30 +1000

    Working osu! requests with bancho irc
    finally

--- lib/osu/banchobot.ex
+++ lib/osu/banchobot.ex
@@ -8,105 +8,43 @@ defmodule Banchobot do
               nick: "Banchobot",
               user: "Banchobot",
               name: "Banchobot",
-              client: nil,
+              bancho_client: nil,
               channels: []
   end
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
-  def init(_type) do
+  def init([]) do
     import Supervisor.Spec, warn: false
 
     config = Application.get_env(:bancho, :irc) |> Enum.into %{}
     config = Map.merge(%State{}, config)
 
-    {:ok, client} = ExIrc.start_client!
+    {:ok, bancho_client} = ExIrc.Client.start_link
 
-    Process.register(client, :bancho_client)
+    Process.register(bancho_client, :bancho_client)
 
-    children = [
-      # Define workers and child supervisors to be supervised
-      worker(BanchoConnectionHandler, [client]),
-      # here's where we specify the channels to join:
-      worker(BanchoLoginHandler, [client, []])
-    ]
+    # children = [
+    #   # Define workers and child supervisors to be supervised
+    #   worker(BanchoConnectionHandler, [bancho_client, config]),
+    #   # here's where we specify the channels to join:
+    #   worker(BanchoLoginHandler, [bancho_client, []])
+    # ]
+    #
+    # # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # # for other strategies and supported options
+    # opts = [strategy: :one_for_one, name: Banchobot.Supervisor]
+    # Supervisor.start_link(children, opts)
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Twitchbot.Supervisor]
-    Supervisor.start_link(children, opts)
+    {:ok, handler} = ExampleHandler.start_link(nil)
+    ExIrc.Client.add_handler(bancho_client, handler)
+    ExIrc.Client.connect!(bancho_client, config.host, config.port)
+    ExIrc.Client.logon(bancho_client, config.pass, config.nick, config.user, config.name)
+
+    {:ok, bancho_client}
   end
 
   def start_link() do
     GenServer.start_link(__MODULE__, [])
-  end
-end
-
-defmodule BanchoConnectionHandler do
-  def start_link(client) do
-    GenServer.start_link(__MODULE__, [client])
-  end
-
-  def init([state]) do
-    ExIrc.Client.add_handler state.client, self
-    ExIrc.Client.connect! state.client, state.host, state.port
-    {:ok, state}
-  end
-
-  def handle_info({:connected, server, port}, state) do
-    debug "Connected to #{server}:#{port}"
-    ExIrc.Client.logon state.client, state.pass, state.nick, state.user, state.name
-    {:noreply, state}
-  end
-
-  def handle_info(:disconnected, state) do
-    debug "Disconnected from #{state.host}:#{state.port}"
-    ExIrc.Client.connect! state.client, state.host, state.port
-    {:noreply, state}
-  end
-
-  # Catch-all for messages you don't care about
-  def handle_info(msg, state) do
-    # debug "Received unknown messsage:"
-    # IO.inspect msg
-    {:noreply, state}
-  end
-
-  defp debug(msg) do
-    IO.puts IO.ANSI.yellow() <> msg <> IO.ANSI.reset()
-  end
-end
-
-
-defmodule BanchoLoginHandler do
-  @moduledoc """
-  This is an example event handler that listens for login events and then
-  joins the appropriate channels. We actually need this because we can't
-  join channels until we've waited for login to complete. We could just
-  attempt to sleep until login is complete, but that's just hacky. This
-  as an event handler is a far more elegant solution.
-  """
-  def start_link(client, channels) do
-    GenServer.start_link(__MODULE__, [client, channels])
-  end
-
-  def init([client, channels]) do
-    ExIrc.Client.add_handler client, self
-    {:ok, {client, channels}}
-  end
-
-  def handle_info(:logged_in, state = {client, channels}) do
-    debug "Logged in to bancho server"
-    channels |> Enum.map(&ExIrc.Client.join client, &1)
-    {:noreply, state}
-  end
-
-  # Catch-all for messages you don't care about
-  def handle_info(_msg, state) do
-    {:noreply, state}
-  end
-
-  defp debug(msg) do
-    IO.puts IO.ANSI.yellow() <> msg <> IO.ANSI.reset()
   end
 end
