adfdcc5639b52e90a15bf9b98778f955d9a220c6
Nicholas Tay <n2468txd@users.noreply.github.com>
2015-07-01 10:35:53 +1000

    Update plugin.ex to use ExRated

--- lib/plugin.ex
+++ lib/plugin.ex
@@ -1,6 +1,7 @@
 defmodule Plugin do
   alias Plugin
-  import Ecto.Query
+
+  import ExRated
 
   def every_x(name, ms, channel, msg) do
     every_x(name, ms, fn ->
@@ -8,25 +9,8 @@ defmodule Plugin do
     end)
   end
   def every_x(name, ms, cb) do
-    q = from t in Database.Timer,
-      where: t.cmd == ^name,
-      select: t
-
-    selection = Twitchbot.Repo.all q
-
-    if selection == [] do
-      t = %Database.Timer{cmd: name, interval: ms}
-      t = Twitchbot.Repo.insert t
-
+    if (ExRated.check_rate(name, ms, 1) |> elem(0)) == :ok do
       cb.()
-
-      Task.start_link(fn ->
-        Stream.timer(ms) |> Enum.take(1) |> delete_timer(t)
-      end)
     end
-  end
-
-  def delete_timer(_s, t) do
-    Twitchbot.Repo.delete t
   end
 end
