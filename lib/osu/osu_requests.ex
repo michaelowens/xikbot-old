9652de9dbc67594a7787338093b82dcd35fe6784
Nicholas Tay <n2468txd@users.noreply.github.com>
2015-07-08 09:36:24 +1000

    Exlude broadcaster from requests

--- lib/osu/osu_requests.ex
+++ lib/osu/osu_requests.ex
@@ -24,9 +24,11 @@ defmodule Twitchbot.OsuRequests do
 
     cond do
       Regex.match?(~r/(http|https):\/\/osu.ppy.sh\/(s|b)\/(\d+)/i, msg) ->
-        osuMatched = Regex.run(~r/(http|https):\/\/osu.ppy.sh\/(s|b)\/(\d+)/i, msg)
-        osuMatched = List.to_tuple(osuMatched)
-        handle_osu_request({channel, user, elem(osuMatched, 2), elem(osuMatched, 3)}, client)
+        if user != String.strip(channel, ?#) do # Ignore broadcaster's map links
+          osuMatched = Regex.run(~r/(http|https):\/\/osu.ppy.sh\/(s|b)\/(\d+)/i, msg)
+          osuMatched = List.to_tuple(osuMatched)
+          handle_osu_request({channel, user, elem(osuMatched, 2), elem(osuMatched, 3)}, client)
+        end
 
       true -> nil
     end
