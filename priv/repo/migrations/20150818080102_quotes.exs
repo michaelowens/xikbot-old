690efa512dfe51eddfa6c1c74f3378c28b5b6407
Nicholas Tay <nexerq@gmail.com>
2015-08-24 17:09:05 +1000

    Instead of varchar, use text for longer texts
    https://github.com/michaelowens/xikbot/pull/15\#issuecomment-134067855

--- priv/repo/migrations/20150818080102_quotes.exs
+++ priv/repo/migrations/20150818080102_quotes.exs
@@ -5,7 +5,7 @@ defmodule Twitchbot.Repo.Migrations.Quotes do
     create table(:quotes) do
       add :channel, :string, size: 255
       add :key, :string, size: 255
-      add :quote, :string, size: 255
+      add :quote, :text
       add :added_by, :string, size: 255
 
       timestamps
